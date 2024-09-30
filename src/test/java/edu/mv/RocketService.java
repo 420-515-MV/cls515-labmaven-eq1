72561fc8e1a606f6d2cd9420294d98ebe272b0f0
MaindyG <112408389+MaindyG@users.noreply.github.com>
2024-09-30 13:44:09 -0400

    Corrections du pom.xml et Nom de classe de Test

---
+++ src/test/java/edu/mv/RocketService.java
@@ -0,0 +1,59 @@
+package edu.mv;
+/*
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+ */
+import edu.mv.models.RocketDTO;
+import edu.mv.persistence.PersistenceService;
+import edu.mv.persistence.RocketNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Unit test for simple App.
+ */
+
+@ExtendWith(MockitoExtension.class)
+public class RocketService {
+    @Mock
+    private PersistenceService persistenceService;
+
+    @InjectMocks
+    private edu.mv.service.RocketService rocketService;
+
+    @Test
+    public void testGetRocket() throws RocketNotFoundException {
+        RocketDTO rocketDTO = new RocketDTO();
+        rocketDTO.setId(1);
+        rocketDTO.setName("Maindy");
+        rocketDTO.setType("F1 Rocket");
+        when(persistenceService.retrieve(1)).thenReturn(rocketDTO);
+        RocketDTO result = rocketService.getRocket(1);
+        assertEquals("Maindy", result.getName());
+    }
+    @Test
+    public void testPutRocket() {
+        RocketDTO rocketDTO = new RocketDTO();
+        rocketDTO.setId(1);
+        rocketDTO.setName("Maindy");
+        rocketDTO.setType("F1 Rocket");
+        rocketService.putRocket(rocketDTO);
+        verify(persistenceService).save(rocketDTO);
+    }
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
+/*
+
+ */
